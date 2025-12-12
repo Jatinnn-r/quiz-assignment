40aa5907a5ae41d026d4049f4c0aa8452eb0e0ac
Jatin Rajput <jatinrajput0666@gmail.com>
2025-12-13 03:32:18 +0530

    Fix QuestionCard component definition

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,3 +1,24 @@
 import React from "react";
+
+interface Props {
+  question: string;
+  options: string[];
+  onSelect: (option: string) => void;
+}
+
+const QuestionCard: React.FC<Props> = ({ question, options, onSelect }) => {
+  return (
+    <div>
+      <h2>{question}</h2>
+      {options.map((opt, index) => (
+        <button key={index} onClick={() => onSelect(opt)}>
+          {opt}
+        </button>
+      ))}
+    </div>
+  );
+};
+
 export default QuestionCard;
+
 export {};
